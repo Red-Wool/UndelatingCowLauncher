475d9020da508fdbf23e2e29a8173bb76607965d
RedWool <leolui0930.sia@gmail.com>
2021-12-14 21:56:37 -0500

    Player Character Added

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,7 +6,20 @@ public class Movement : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed;
+    public Transform cam;
+
+    public float jumpPower;
+
+    private float gravity;
+    public float gravityPower;
+    public float gravityMax;
+
+    private float speed;
+    public float speedAccel;
+    public float speedDeccel;
+    public float maxSpeed;
+
+    public float turnSmooth;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +36,39 @@ public class Movement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f)
         {
-            float lookAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, lookAngle, 0f);
+            speed = Mathf.Clamp(speed + Time.deltaTime * speedAccel, 0f, maxSpeed);
 
-            controller.Move(direction * speed * Time.deltaTime);
+            float lookAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, lookAngle, ref turnSmooth, 0.1f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = (Quaternion.Euler(0f, lookAngle, 0f) * Vector3.forward).normalized;
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+        else
+        {
+            speed = Mathf.Clamp(speed - Time.deltaTime * speedDeccel, 0f, maxSpeed);
+        }
+
+
+        if (Physics.Raycast(transform.position, Vector3.down, 1.1f))
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                gravity += jumpPower;
+            }
+            else
+            {
+                gravity = 0f;
+            }
+        }
+        else
+        {
+            gravity = Mathf.Max(gravity - Time.deltaTime * gravityPower, -gravityMax);
+        }
+
+        controller.Move(Vector3.up * gravity * Time.deltaTime);
+
     }
 }
